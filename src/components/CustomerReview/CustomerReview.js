366139f42d76846fc61aaf9a460af706908487ff
abubakarsiddique10 <abubakarsiddique7912@gmail.com>
2022-04-04 13:39:00 +0600

    customer review show name

--- src/components/CustomerReview/CustomerReview.js
+++ src/components/CustomerReview/CustomerReview.js
@@ -1,11 +1,43 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import './CustomerReview.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faStar } from '@fortawesome/free-solid-svg-icons'
+import { Card, Col, Row } from "react-bootstrap";
 const CustomerReview = (props) => {
-    const { name, id } = props.review;
+    const { name, review } = props.review;
     return (
         <Col>
-            <h3>{name}</h3>
+            <Card>
+                {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
+                <Card.Body className="text-start">
+                    <div className="font-icon">
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                    </div>
+                    <Card.Title className="my-2">{name}</Card.Title>
+                    <Card.Text>{review}</Card.Text>
+                </Card.Body>
+            </Card>
         </Col>
     )
 }
 export default CustomerReview;
+
+
+
+
+
+{/* <Col className="text-start">
+            <div className="font-icon">
+                <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+            </div>
+            <h5>{name}</h5>
+            <p>{review}</p>
+        </Col> */}
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,22 +15,23 @@ const Home = () => {
                         <p className="home-info">If you’re looking for great value second hand books, then World of Books is the place for you.
                             As you may already know, we aren’t like other online book stores. For starters, we don’t believe that books should only
                             be read once, or have a single owner.</p>
-                        <Button className="home-btn">Explore More</Button>
+                        <Button className="home-btn px-5 m-0">Explore More</Button>
                     </Col>
                     <Col className="ms-4">
-                        <Image style={{ width: '600px' }} src="https://www.bookbaby.com/Images/novel-thumb-pocket-soft.png"></Image>
+                        <Image className="img-fluid" style={{ width: '600px' }} src="https://www.bookbaby.com/Images/novel-thumb-pocket-soft.png"></Image>
                     </Col>
                 </Row>
             </Container>
             <Container className="my-5">
-                <Row className="row-cols-3">
+                <h1 className="mb-3">Customers Reviews</h1>
+                <Row xs={1} md={2} lg={3} className="g-4">
                     {
                         reviews.map(review => <CustomerReview key={review.id} review={review}></CustomerReview>)
                     }
                 </Row>
+                <Button className="mt-3 w-50">See All Reviews</Button>
             </Container>
         </div>
     )
 }
 export default Home;
-
